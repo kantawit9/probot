dc3b2594046fa961b2b462e4cf4a301f10d5aa6f
kantawit9 <51392815+kantawit9@users.noreply.github.com>
2019-06-05 10:57:25 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,8 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'o2d89p0fqn5Gt6oxinMpVxPbynAx+Q//Y1ZGaUkATJM025mD/9/X5VYUYo9rxpjfqMqdHsUkv3/qSc0By4HFLvSIS8SbSJQMdn915KZPGg+WP0OW3z5cAYK3vnIV9S6/RXOCby21ETN6ojoJLf0ltQdB04t89/1O/w1cDnyilFU=
+';
 
 // Get POST body content
 $content = file_get_contents('php://input');
